3afe6fdd077443111187956a9e5410761d9b906d
KrishN-hub <krishnakumarpirunthavan@gmail.com>
2025-01-22 16:36:09 +0530

    Mastering Flutter: Essential Utilities, Helper Functions, Constants and much more

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
+import 'package:e_commerce/app.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+////------------------------- Entry point of  Flutter App----------
 
 void main() {
-  runApp(App());
-}
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme
-    );
+  //Todo: Add Widgets Bindings 
+  //Todo: Init Local Storage
+  //Todo: Await Native Splash
+  //Todo: Initialize Firebase
+  //Todo: Initialize Authentication
 
-  }
+  runApp(const App());
 }
+
+///----Use this Class to setup themes, initial bindings, any animations and much more using Material Widgets--------
+
